98962065d02b3bf3a1e59cec12baa87bf3d0dcf8
Shubham6865 <paradeshubham6865@gmail.com>
2024-09-15 17:04:30 +0530

    issue with deploy

--- src/images/index.js
+++ src/images/index.js
@@ -1,6 +1,6 @@
 export { default as logo } from "./logo.png"
 export { default as profile1 } from "./profile-1.png"
-export { default as profile2 } from "./profile-2.JPG"
+export { default as profile2 } from "../images/profile-2.JPG"
 // export { default as profile3 } from "./profile-3.jpeg"
 // export { default as profile4 } from "./profile-4.jpeg"
 // export { default as profile5 } from "./profile-5.jpeg"
